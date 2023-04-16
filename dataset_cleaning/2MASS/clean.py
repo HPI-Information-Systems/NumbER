36b9dcb926450a17ef8ba9ed332a8c31718c6558
lasklu <lukas.laskowski@student.hpi.de>
2023-04-16 20:01:34 +0200

    modifying clean script 2MASS

--- dataset_cleaning/2MASS/clean.py
+++ dataset_cleaning/2MASS/clean.py
@@ -7,32 +7,45 @@ def read_file(path, columns_path):
 	def get_columns(line):
 		return line.split()[0]
 	columns = list(map(get_columns, columns))
+	print(columns)
 	df = pd.read_csv(path, sep='|', names=columns)
 	return df
-xsc = read_file('/hpi/fs00/share/fg/naumann/lukas.laskowski/2MASS/samples/xsc/100_xsc', 'xsc_columns')
-psc = read_file('/hpi/fs00/share/fg/naumann/lukas.laskowski/2MASS/samples/100_2MASS.txt', 'psc_columns')
+xsc = read_file('xsc_sample', 'xsc_columns')
+psc = read_file('psc_sample', 'psc_columns')
+#print(psc.columns)
+#print(xsc.columns)
 xsc['source'] = 'xsc'
 psc['source'] = 'psc'
 #rename columns for xsc
-xsc.rename(columns={'j_msig_k20fe': 'j_msig',
+#xsc.drop(['ext_key'], inplace=True, axis=1)
+xsc.rename(columns={'j_msig_k20fe': 'j_msigcom',
                    'j_m_k20fe': 'j_m',
                    'k_m_k20fe': 'k_m',
-                   'k_msig_k20fe': 'k_msig',
+                   'k_msig_k20fe': 'k_msigcom',
                    'h_m_k20fe': 'h_m',
-                   'h_msig_k20fe': 'h_msig',
-                   'jdate':'j_date'
+                   'h_msig_k20fe': 'h_msigcom',
+                   #'scan_key': 'ext_key',
                    },
           inplace=True, errors='raise')
-psc = psc[['source', 'id', 'ra', 'dec','designation','glat', 'glon','hemis','date','j_date', 'j_m', 'j_msigcom', 'h_m', 'h_msigcom', 'k_m', 'k_msigcom']]
-xsc = xsc[['source', 'id', 'ra', 'dec','designation','glat', 'glon','hemis','date','j_date', 'j_m', 'j_msigcom', 'h_m', 'h_msigcom', 'k_m', 'k_msigcom']]
+#xsc.to_csv('temp.csv', index=False)
+print("DSds")
+print(psc[psc['ext_key']==2000451]['ra'])
+print("HDS")
+#print("D)WDWDW")
+psc = psc[['source','ext_key', 'ra', 'decl','designation','glat', 'glon','hemis','date','jdate', 'j_m', 'j_msigcom', 'h_m', 'h_msigcom', 'k_m', 'k_msigcom']]
+#return
+xsc = xsc[['source', 'ext_key','ra', 'decl','designation','glat', 'glon','hemis','date','jdate', 'j_m', 'j_msigcom', 'h_m', 'h_msigcom', 'k_m', 'k_msigcom']]
 
 all_stars = pd.concat([xsc, psc])
 groundtruth = []
-for row in all_stars[all_stars['source']=='psc'].iterrows(all_stars):
-    xsc_row = all_stars[row['id'] == all_stars['id'] & all_stars['source'] == 'xsc']
+for idx,row in psc.iterrows():
+    if row['ext_key'] == 54361:
+        #continue
+        pass
+    xsc_row = all_stars[row['ext_key'] == all_stars['ext_key']]
+    xsc_row = xsc_row[xsc_row['source'] == 'xsc']
+    #print(all_stars[all_stars['source'] == 'xsc'])
     assert len(xsc_row) == 1
-    idx_xsc = xsc_row.loc[:,0].index
-    idx_psc = row.loc[:,0].index
-    groundtruth.append((idx_xsc, idx_psc))
+    groundtruth.append((xsc_row.index[0], idx))
 groundtruth = pd.DataFrame(groundtruth, columns=['xsc', 'psc'])
 groundtruth.to_csv('groundtruth.csv', index=False)
