95c521dbed42eb920c27cf9f1464c8ee3d31c976
lasklu <lukas.laskowski@student.hpi.de>
2023-04-17 07:46:42 +0200

    modifiyng dataset creation

--- dataset_cleaning/proteins/dataset_creation.py
+++ dataset_cleaning/proteins/dataset_creation.py
@@ -9,7 +9,7 @@ output_path = ""
 with open("all_clusters.json", "r") as f:
     clusters = json.load(f)
 result = []#pd.DataFrame(columns=["name","lig_cov","poc_cov","lig_name","4A_crit","ligSASRatio","volume","enclosure","surface","lipoSurface",	"depth",	"surf/vol",	"lid/hull",	"ellVol",	"ell_c/a",	"ell_b/a",	"surfGPs",	"lidGPs",	"hullGPs",	"siteAtms",	"accept",	"donor",	"aromat",	"hydrophobicity",	"metal",	"Cs",	"Ns",	"Os",	"Ss",	"Xs",	"acidicAA",	"basicAA",	"polarAA",	"apolarAA",	"sumAA",	"ALA",	"ARG",	"ASN",	"ASP",	"CYS",	"GLN",	"GLU",	"GLY",	"HIS",	"ILE",	"LEU",	"LYS",	"MET",	"PHE",	"PRO",	"SER",	"THR",	"TRP",	"TYR",	"VAL",	"A",	"C",	"G",	"U",	"I",	"N",	"DA",	"DC",	"DG",	"DT",	"DN",	"UNK"])
-for cluster in clusters:
+for key, cluster in clusters.items():
     for pdb_id in cluster:
         if not os.path.exists(f"{path_to_pdb_files}/{pdb_id}.pdb"):
             os.system(f"wget https://files.rcsb.org/download/{pdb_id}.pdb -P {path_to_pdb_files}")
@@ -18,6 +18,7 @@ for cluster in clusters:
         data = pd.read_csv(f"{output_path}/{pdb_id}.dogsite_desc.txt", sep="\t")
         means = data[["lig_cov","poc_cov","lig_name","4A_crit","ligSASRatio","volume","enclosure","surface","lipoSurface",	"depth",	"surf/vol",	"lid/hull",	"ellVol",	"ell_c/a",	"ell_b/a",	"surfGPs",	"lidGPs",	"hullGPs",	"siteAtms",	"accept",	"donor",	"aromat",	"hydrophobicity",	"metal",	"Cs",	"Ns",	"Os",	"Ss",	"Xs",	"acidicAA",	"basicAA",	"polarAA",	"apolarAA",	"sumAA",	"ALA",	"ARG",	"ASN",	"ASP",	"CYS",	"GLN",	"GLU",	"GLY",	"HIS",	"ILE",	"LEU",	"LYS",	"MET",	"PHE",	"PRO",	"SER",	"THR",	"TRP",	"TYR",	"VAL",	"A",	"C",	"G",	"U",	"I",	"N",	"DA",	"DC",	"DG",	"DT",	"DN",	"UNK"]].mean().to_dict()
         means['name'] = pdb_id
+        means['cluster'] = key
         result.append(means)
 result = pd.DataFrame(result)
 result.to_csv("dogsite.csv", index=False)
